4d793d57adcbb6092e41b83efb56b8a3f35f9c6d
ffischer42 <info@felixfischer.io>
2024-04-18 11:29:07 +0200

    Initial commit. Adding plugin description.

---
+++ allpost-reservations.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: AllPost Reservations
+ * Plugin URI: https://github.com/ffischer42/AllPost-Reservations
+ * Description: Making WP event registration simple again.
+ * Version: dev
+ * Author: Felix Fischer
+ * Author URI: https://github.com/ffischer42
+ */
